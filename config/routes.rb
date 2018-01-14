9a20e8ea3ad20fd49f0b711880ca147323fdd7b9
cloudy9101 <cloudy9101@gmail.com>
2018-01-14 17:28:49 +0800

    ADD: Add a index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#show'
+  root 'static_pages#index'
   use_doorkeeper
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: :create do
